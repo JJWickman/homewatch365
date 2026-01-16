8e01470dd16877a5589933fa04ad67acb771c309
jasonwi <jasonwi@live.com>
2026-01-16 21:44:39 +0000

    File changes

--- src/pages/Settings.jsx
+++ src/pages/Settings.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, Link } from 'react-router-dom';
 import { base44 } from '@/api/base44Client';
 import { createPageUrl } from '@/utils';
 import { 
