5b6a934aca782a214cfd4fd6d9905150198cf595
jasonwi <jasonwi@live.com>
2026-01-16 20:58:56 +0000

    File changes

--- src/pages/PropertyForm.jsx
+++ src/pages/PropertyForm.jsx
@@ -649,37 +649,6 @@ export default function PropertyForm() {
     }
   };
 
-  const handleChange = (field, value) => {
-    setFormData(prev => ({ ...prev, [field]: value }));
-  };
-
-  const handleUtilityChange = (field, value) => {
-    setFormData(prev => ({
-      ...prev,
-      utilities: { ...prev.utilities, [field]: value }
-    }));
-  };
-
-  const addEmergencyContact = () => {
-    setFormData(prev => ({
-      ...prev,
-      emergency_contacts: [...prev.emergency_contacts, { name: '', relationship: '', phone: '', email: '' }]
-    }));
-  };
-
-  const updateEmergencyContact = (index, field, value) => {
-    const contacts = [...formData.emergency_contacts];
-    contacts[index] = { ...contacts[index], [field]: value };
-    setFormData(prev => ({ ...prev, emergency_contacts: contacts }));
-  };
-
-  const removeEmergencyContact = (index) => {
-    setFormData(prev => ({
-      ...prev,
-      emergency_contacts: prev.emergency_contacts.filter((_, i) => i !== index)
-    }));
-  };
-
   if (loading) {
     return (
       <div className="flex items-center justify-center min-h-[50vh]">
