84821825df0a5c0dfe34c5bfe474d58cf6bc08f3
jasonwi <jasonwi@live.com>
2026-01-19 16:40:52 +0000

    File changes

--- src/pages.config.js
+++ src/pages.config.js
@@ -22,6 +22,7 @@ import Schedule from './pages/Schedule';
 import Settings from './pages/Settings';
 import StripeSetup from './pages/StripeSetup';
 import TestGoogleMapsAPI from './pages/TestGoogleMapsAPI';
+import Home from './pages/Home';
 import __Layout from './Layout.jsx';
 
 
@@ -50,6 +51,7 @@ export const PAGES = {
     "Settings": Settings,
     "StripeSetup": StripeSetup,
     "TestGoogleMapsAPI": TestGoogleMapsAPI,
+    "Home": Home,
 }
 
 export const pagesConfig = {
