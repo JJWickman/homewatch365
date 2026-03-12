3557e0304550cc6c911586e2ab0ea20a3dbb7cdc
jasonwi <jasonwi@live.com>
2026-03-12 16:51:06 +0000

    File changes

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import NavigationTracker from '@/lib/NavigationTracker'
 import { pagesConfig } from './pages.config'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import PageNotFound from './lib/PageNotFound';
+import ChecklistEditor from './pages/ChecklistEditor';
 import { AuthProvider, useAuth } from '@/lib/AuthContext';
 import UserNotRegisteredError from '@/components/UserNotRegisteredError';
 
