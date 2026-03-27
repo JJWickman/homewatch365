3cf9e45811e6df7622bec5581e75c24161d55997
jasonwi <jasonwi@live.com>
2026-03-27 15:25:12 +0000

    File changes

--- base44/functions/createCheckoutSession/entry.ts
+++ base44/functions/createCheckoutSession/entry.ts
@@ -5,7 +5,8 @@ const stripe = new Stripe(Deno.env.get('STRIPE_SECRET_KEY'));
 
 Deno.serve(async (req) => {
   try {
-    const base44 = createClientFromRequest(req);
+    const appId = Deno.env.get('BASE44_APP_ID');
+    const base44 = createClientFromRequest(req, { appId });
     const user = await base44.auth.me();
     
     if (!user) {
