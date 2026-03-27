38a21946d6823dc0ded985a4278dc66b0c320f6b
jasonwi <jasonwi@live.com>
2026-03-27 15:52:59 +0000

    File changes

--- src/api/base44Client.js
+++ src/api/base44Client.js
@@ -1,9 +1,11 @@
 import { createClient } from '@base44/sdk';
 import { appParams } from '@/lib/app-params';
 
-const { appId, token, functionsVersion, appBaseUrl } = appParams;
+const { appId: paramAppId, token, functionsVersion, appBaseUrl } = appParams;
 
 //Create a client with authentication required
+const appId = paramAppId || '696806e88e744d6cc803e3bb';
+
 export const base44 = createClient({
   appId,
   token,
